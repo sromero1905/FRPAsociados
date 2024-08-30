4c3247eae678622b0eee182fda9978e24585f457
sromero1905 <santirom1905@gmail.com>
2024-08-29 21:46:08 -0300

    se soluciona el problema de transicion y se termina la version piloto del about section

--- components/aboutus/VisionMissionValuesSection.tsx
+++ components/aboutus/VisionMissionValuesSection.tsx
@@ -2,33 +2,33 @@ import { motion } from 'framer-motion';
 
 const VisionMissionValuesSection = () => {
   return (
-    <section className="vision-mission-values-section">
+    <section className="vision-mission-values-section mb-14">
       <motion.div
         className="motion-div"
-        initial={{ opacity: 0, y: 20 }}
+        initial={{ opacity: 0, y: 40 }}
         whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
-        transition={{ duration: 1.2, ease: "easeInOut" }}
+        viewport={{ once: true, amount: 0.2 }}
+        transition={{ duration: 1.5, ease: 'easeInOut' }}
       >
         <h2 className="section-title">Visión</h2>
         <p className="section-text">Proteger y generar riqueza a nuestros clientes en el mercado inmobiliario.</p>
       </motion.div>
       <motion.div
         className="motion-div"
-        initial={{ opacity: 0, y: 20 }}
+        initial={{ opacity: 0, y: 40 }}
         whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
-        transition={{ duration: 1.2, ease: "easeInOut" }}
+        viewport={{ once: true, amount: 0.2 }}
+        transition={{ duration: 1.5, ease: 'easeInOut' }}
       >
         <h2 className="section-title">Misión</h2>
         <p className="section-text">Ser el resguardo legal que les permita a nuestros clientes proteger sus derechos y generar riqueza en el ámbito inmobiliario.</p>
       </motion.div>
       <motion.div
         className="motion-div"
-        initial={{ opacity: 0, y: 20 }}
+        initial={{ opacity: 0, y: 40 }}
         whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
-        transition={{ duration: 1.2, ease: "easeInOut" }}
+        viewport={{ once: true, amount: 0.2 }}
+        transition={{ duration: 1.5, ease: 'easeInOut' }}
       >
         <h2 className="section-title">Valores</h2>
         <p className="section-text">Honestidad | Sinceridad | Compromiso</p>
