f6cbf8e8cbc1119f3b1a1da995e98dab0a8c6b1c
sromero1905 <santirom1905@gmail.com>
2024-09-03 22:44:32 -0300

    se agregan imgs y se mejra el hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "FRPA Asociados",
+  description: "Romero Porres y Asociados es un estudio jurídico especializado en derecho inmobiliario, abarca la universalidad de situaciones y problemáticas que acontecen en lo relacionado a los inmuebles en sus diversas manifestaciones.",
 };
 
 export default function RootLayout({
